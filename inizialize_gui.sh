a650b1de48f436527c586d7d6886e1dc57ed22f2
ZioCook <ZioCook@users.noreply.github.com>
2026-09-16 15:48:02 +0200

    Fix version string bug in build scripts

--- inizialize_gui.sh
+++ inizialize_gui.sh
@@ -103,7 +103,7 @@ done
 
 # Inject build version into rootdevice
 short_commit=$(git rev-parse --short HEAD 2>/dev/null || echo "dev")
-build_ver="${VERSION:-9.7.9}"
+build_ver="${version:-9.7.9}"
 # If VERSION already contains a hyphen/commit hash, do not append short_commit again
 if [[ "$build_ver" =~ -[0-9a-fA-F]{7,8}$ ]] || [[ "$build_ver" =~ -dev$ ]]; then
 	stamp_ver="$build_ver"
